f30f1cd92a49003c784b67469a8287717048102b
Dirk Persky <dearic@gmx.net>
2012-05-15 13:05:00 +0200

    auslagern der js in utility.js

--- js/utility.js
+++ js/utility.js
@@ -44,3 +44,89 @@ function KalenderWoche(j,m,t) {
     }
     return kw;
 }
+
+function DynTable(TableDivID, ListBoxID, JSONobj){
+// auslesen der Spalten und definieren der Checkboxen
+    var alltr=jQuery('#'+TableDivID+' table tr');
+    jQuery(alltr).each( function(index,value){
+      var allth=jQuery(this).find('th');
+      var container2 = "";
+      $(allth).each(function(cindex,value){
+         container2 += '<li><input id="checkbox_'+index+'_'+cindex+'" type="checkbox" checked="checked" /><label for="checkbox_'+index+'_'+cindex+'" >'+$(this).text()+'</label></li>';
+         $('#'+ListBoxID+' ul').html(container2);
+		 $('#'+ListBoxID+' ul li:last-child').css('display','none')
+      });
+    });
+    // ein und ausblenden der Spalten
+    var allCBox = $('#'+ListBoxID+' ul li input');
+    $(allCBox).each(function(cindex,value) {
+        var pos = cindex+1;
+        //console.log(cindex+1);
+        $(this).click(function (){
+            $('th:nth-child('+pos+')').toggle();
+            $('td:nth-child('+pos+')').toggle();
+        });
+    });
+	
+	$('#'+TableDivID+' table th:last-child').bind('click',function(e){
+		var _left = $(this).position().left;
+		$('#'+ListBoxID+'').animate({
+			left:_left,
+			top:$('#'+TableDivID+' table tr:eq(1) td:last-child').position().top,
+		},0);
+		$('#'+ListBoxID+'').toggle();
+		return false;
+	});
+
+	var timer;
+	$('#'+ListBoxID+' ul').bind('mouseout', function(){
+		if(timer)
+			clearTimeout(timer);
+		timer = setTimeout("$('#"+ListBoxID+"').toggle()",100);
+	});
+	$('#'+ListBoxID+' ul').bind('mouseover', function(){
+		clearTimeout(timer);
+	});
+	
+	// Default werte für Tabs
+	var _checked = new Array(0,1);
+	
+	var _obj = new Object();
+	if(localStorage){
+		if(localStorage.getItem('vqc')){
+			var myJson = localStorage.getItem('vqc');
+			 _obj = jQuery.parseJSON(myJson);
+		}
+	}
+	
+	if(_obj[JSONobj]){
+		_checked = new Array();
+		for(var index in _obj[JSONobj]){
+			_checked.push(_obj[JSONobj][index]);
+		}
+	}
+	for(var i = 0 ; i< ($('#'+ListBoxID+' ul li input').length-1);i++){
+		var _temp = true;
+		for(var j = 0 ;j<_checked.length;j++)
+			if(i==_checked[j]) _temp = false;
+		if(_temp)
+			$('#checkbox_0_'+i).trigger('click');
+	}
+	
+	$('#'+ListBoxID+' ul li input').bind('click',function(){
+		for(var i = 0; i < ($('#'+ListBoxID+' ul li input').length-1);i++){
+			var _id = $('#'+ListBoxID+' ul li input:eq('+i+')').attr('id');
+			if(!_obj[JSONobj])
+				_obj[JSONobj] = new Object();
+			if($('#'+ListBoxID+' ul li input:eq('+i+')').attr('checked')){
+				_obj[JSONobj][_id]=i;
+			}else{
+				delete _obj[JSONobj][_id];
+			}
+		}
+		var _JSON = JSON.stringify(_obj);
+		if(localStorage){
+			localStorage.setItem('vqc',_JSON);
+		}
+	});
+}
